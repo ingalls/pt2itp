05ce45415275be83cc629c3cbf2e7962ae6d4991
kdurril <kdurril@gmail.com>
2017-10-18 16:13:21 -0400

    added 2 tests, error & ok to testcsv.test.js, testidx points to absolute path, user must change path when given working input file.

--- test/testcsv.test.js
+++ test/testcsv.test.js
@@ -1,41 +1,14 @@
 const test = require('tape');
 const testcsv = require('../lib/testcsv');
 
-
-/*test('edge cases - empty string', (t) => {
-    t.deepEqual(tokenize.main(''), []);
-    t.end();
-});
-*/
-
-/*40, 80, "Main street" \n*/
-
-const testobj = { _: 
-   [ '/Users/mapbox/.nvm/versions/node/v6.10.2/bin/node',
-     '/usr/local/bin/pt2itp',
-     'testcsv' ],
-  'test-ephermal': false,
-  index: '/Users/mapbox/Developer/us_ri-address-both-0d603c2a171017011038-0d603c2a39/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles',
-  i: '/Users/mapbox/Developer/us_ri-address-both-0d603c2a171017011038-0d603c2a39/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles',
-  query: 'Greenview St' };
+const testidx = "/Users/mapbox/Developer/us_ri-address-both-0d603c2a171017011038-0d603c2a39/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles";
 
 test('testcsv', (t) => {
-	testcsv(testobj , (err, res) => {
-        console.log('err',err);
-        console.log('res',res);
+
+	testcsv({index: testidx, query:'greenview'} , (err, res) => {
+        t.error(err);
+        t.ok(res.features.length > 0);
+
 	})
 	t.end();
 });
-
-
-/*
-    tape('test address index for alphanumerics', (t) => {
-        c.geocode('9B FAKE STREET', { limit_verify: 1 }, (err, res) => {
-            t.ifError(err);
-            t.equals(res.features[0].place_name, '9b fake street', 'found 9b fake street');
-            t.equals(res.features[0].relevance, 1.00);
-            t.end();
-        });
-    });
-
-*/
