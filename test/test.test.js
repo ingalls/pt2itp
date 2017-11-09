20f76b667fd8586c1ea8c94d2a5c614195fea108
Lizzie Gooding <lizzie.gooding@mapbox.com>
2017-11-09 13:49:51 -0500

    fix failing stderr test, clean up tape syntax

--- test/test.test.js
+++ test/test.test.js
@@ -1,7 +1,7 @@
 const Index = require('../lib/index');
 const worker = require('../lib/map');
 
-const tape = require('tape');
+const test = require('tape');
 const spawn = require('tape-spawn');
 const csv = require('fast-csv');
 const pg = require('pg');
@@ -20,15 +20,15 @@ const pool = new pg.Pool({
 
 const index = new Index(pool);
 
-tape.test('Drop/init database', (t) => {
+test('Drop/init database', (t) => {
     index.init((err, res) => {
         t.error(err);
         t.end();
     });
 });
 
-// loads address and network data into postgres, produces an ITP geojson in /tmp/
-tape.test('load address and network files', (t) => {
+// loads address and network data into postgres
+test('load address and network files', (t) => {
     worker({
             'in-address': './test/fixtures/index-ri-single/address.geojson',
             'in-network': './test/fixtures/index-ri-single/network.geojson',
@@ -42,24 +42,14 @@ tape.test('load address and network files', (t) => {
     });
 });
 
-tape.test('Run test mode', (t) => {
+test('Run test mode', (t) => {
 
     let st = spawn(t, `${__dirname}/../index.js test --index ${carmenIndex} --database ${database} --output ${output} --config ${config}`);
 
     t.test('Return correct std.err message', (t) => {
-        st.stderr.match(`
-            ok - beginning match
-            ok - beginning unmatch
-            ok - beginning diff name
-
-
-            ERROR TYPE                   COUNT
-            -----------------------------------------------------------------------------------
-            NAME MISMATCH (SOFT)             1 ( 50.0% of errors |  9.1% of total addresses)
-            NO RESULTS                       1 ( 50.0% of errors |  9.1% of total addresses)
-
-            ok - 1/11 (9.1%) failed to geocode
-            `.replace(/^ +/mg, ''));
+        st.stderr.match(/NAME MISMATCH \(SOFT\)\s+1 \( 50\.0% of errors \|  9\.1% of total addresses\)/, 'NAME MISMATCH (SOFT) error');
+        st.stderr.match(/NO RESULTS\s+1 \( 50\.0% of errors \|  9\.1% of total addresses\)/, 'NO RESULTS error');
+        st.stderr.match(/1\/11 \(9\.1%\) failed to geocode/, 'failed to geocode error')
         st.end();
     });
 
@@ -76,21 +66,18 @@ tape.test('Run test mode', (t) => {
             t.notEqual(csvErr.map((ele) => ele.error).indexOf('NAME MISMATCH (SOFT)'), -1);
             t.end();
         });
-
         st.end();
     });
 });
 
-
-//comment out to persist database after test run
-tape.test('Drop/init database', (t) => {
+test('Drop/init database', (t) => {
     index.init((err, res) => {
         t.error(err);
         t.end();
     });
 });
 
-tape.test('end connection', (t) => {
+test('end connection', (t) => {
     pool.end();
     t.end();
 });
