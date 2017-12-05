e0c2b3be647271de1279c73805626c3d48bdf9a8
ingalls <ingalls@protonmail.com>
2017-12-05 10:34:33 -0500

    Minor formatting fixtures

--- test/test.test.js
+++ test/test.test.js
@@ -1,15 +1,16 @@
 const Index = require('../lib/index');
 const worker = require('../lib/map');
 
-const test = require('tape');
 const spawn = require('tape-spawn');
 const csv = require('fast-csv');
+const test = require('tape');
+const path = require('path');
 const pg = require('pg');
 
-const carmenIndex = `${__dirname}/fixtures/index-ri-single/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles`;
 const database = 'pt_test';
-const output = `${__dirname}/fixtures/index-ri-single/test-mode-ri-errors.csv`;
-const config = `${__dirname}/fixtures/index-ri-single/index-ri-carmen-config.json`;
+const carmenIndex = path.resolve(__dirname, './fixtures/index-ri-single/us_ri-address-both-0d603c2a171017011038-0d603c2a39.mbtiles');
+const output = path.resolve(__dirname, './fixtures/index-ri-single/test-mode-ri-errors.csv');
+const config = path.resolve(__dirname, './fixtures/index-ri-single/index-ri-carmen-config.json');
 
 const pool = new pg.Pool({
     max: 10,
@@ -30,20 +31,19 @@ test('Drop/init database', (t) => {
 // loads address and network data into postgres
 test('load address and network files', (t) => {
     worker({
-            'in-address': './test/fixtures/index-ri-single/address.geojson',
-            'in-network': './test/fixtures/index-ri-single/network.geojson',
-            output: '/tmp/itp.geojson',
-            debug: true,
-            db: 'pt_test',
-            tokens: 'en'
-       }, (err, res) => {
-            t.error(err);
-            t.end();
+        'in-address': './test/fixtures/index-ri-single/address.geojson',
+        'in-network': './test/fixtures/index-ri-single/network.geojson',
+        output: '/tmp/itp.geojson',
+        debug: true,
+        db: database,
+        tokens: 'en'
+    }, (err, res) => {
+        t.error(err);
+        t.end();
     });
 });
 
 test('Run test mode', (t) => {
-
     let st = spawn(t, `${__dirname}/../index.js test --index ${carmenIndex} --database ${database} --output ${output} --config ${config}`);
 
     t.test('Return correct std.err message', (t) => {
@@ -57,10 +57,10 @@ test('Run test mode', (t) => {
         let csvErr = [];
 
         csv.fromPath(output, {headers: true})
-        .on("data", (data) => {
+        .on('data', (data) => {
             csvErr.push(data);
         })
-        .on("end", function() {
+        .on('end', function() {
             t.equal(csvErr.length, 2);
             t.notEqual(csvErr.map((ele) => ele.error).indexOf('NO RESULTS'), -1);
             t.notEqual(csvErr.map((ele) => ele.error).indexOf('NAME MISMATCH (SOFT)'), -1);
