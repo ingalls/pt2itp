f75cb79001764f63ab2cde3d93cb37ae65ff3657
ingalls <ingalls@protonmail.com>
2019-06-06 12:08:27 -0400

    Restore intersection tests

--- native/src/pg/intersections.rs
+++ native/src/pg/intersections.rs
@@ -11,7 +11,7 @@ impl Intersections {
     ///
     /// Create intersections from network data
     ///
-    pub fn generate(&self, conn: &postgres::Connection) {
+    pub fn generate(&self, conn: &Connection) {
         conn.execute("
             INSERT INTO intersections (a_id, b_id, geom) (
                 SELECT
@@ -33,6 +33,13 @@ impl Intersections {
                 FROM network_cluster
                 WHERE intersections.b_id = network_cluster.id
         ", &[]).unwrap();
+
+        conn.execute("
+            UPDATE intersections
+                SET a_street = network_cluster.names
+                FROM network_cluster
+                WHERE intersections.a_id = network_cluster.id
+        ", &[]).unwrap();
     }
 }
 
@@ -48,15 +55,17 @@ impl Table for Intersections {
 
         conn.execute(r#"
             CREATE UNLOGGED TABLE intersections (
+                id SERIAL,
                 a_id BIGINT,
                 b_id BIGINT,
+                a_street JSONB,
                 b_street JSONB,
                 geom GEOMETRY(POINT, 4326)
             )
         "#, &[]).unwrap();
     }
 
-    fn count(&self, conn: &postgres::Connection) -> i64 {
+    fn count(&self, conn: &Connection) -> i64 {
         match conn.query("
             SELECT count(*) FROM intersections
         ", &[]) {
