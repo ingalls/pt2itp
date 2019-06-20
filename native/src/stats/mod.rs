7cca26e637ae2023669832390c67786d73a25c3f
ingalls <ingalls@protonmail.com>
2019-06-20 08:40:44 -0400

    Add new custom stats

--- native/src/stats/mod.rs
+++ native/src/stats/mod.rs
@@ -23,31 +23,6 @@ impl StatsArgs {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug)]
-pub struct Stats {
-    feats: i64, // Total number of features
-    clusters: i64, // Total number of addr/network clusters
-    invalid: i64, // Total number of unrecognized features (not counted in feats)
-    addresses: i64, // Total number of address points in clusters/orphans
-    intersections: i64, // Total number of address features
-    address_orphans: i64, // Total number of address orphans
-    network_orphans: i64 // Total number of network orphans
-}
-
-impl Stats {
-    fn new() -> Self {
-        Stats {
-            feats: 0,
-            clusters: 0,
-            invalid: 0,
-            addresses: 0,
-            intersections: 0,
-            address_orphans: 0,
-            network_orphans: 0
-        }
-    }
-}
-
 pub fn stats(mut cx: FunctionContext) -> JsResult<JsValue> {
     let args: StatsArgs = match cx.argument_opt(0) {
         None => StatsArgs::new(),
@@ -61,7 +36,7 @@ pub fn stats(mut cx: FunctionContext) -> JsResult<JsValue> {
         }
     };
 
-    let mut boundmap: HashMap<String, String> = HashMap::new();
+    let mut boundmap: HashMap<String, Stats> = HashMap::new();
 
     let is_bounded = args.bounds.is_some();
 
@@ -74,6 +49,11 @@ pub fn stats(mut cx: FunctionContext) -> JsResult<JsValue> {
             let feat = match bound {
                 geojson::GeoJson::Feature(feat) => feat,
                 _ => panic!("Bounds must be (Multi)Polygon Features")
+            };
+
+            let name = match feat.properties.unwrap().get(&String::from("name")) {
+                Some(name) => name,
+                None => panic!("Add bounds features must have .properties.name string")
             };
 
             let geom: geo::Geometry<f64> = feat.geometry.unwrap().value.try_into().unwrap();
@@ -89,6 +69,7 @@ pub fn stats(mut cx: FunctionContext) -> JsResult<JsValue> {
             tree_contents.push(rect);
         }
     }
+
     let mut tree = rstar::RTree::bulk_load(tree_contents);
 
     let feat_stream = GeoStream::new(args.input);
@@ -254,5 +235,65 @@ impl rstar::RTreeObject for Rect {
             [self.bound.min.x, self.bound.min.y],
             [self.bound.max.x, self.bound.max.y]
         )
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Stats {
+    feats: i64, // Total number of features
+    clusters: i64, // Total number of addr/network clusters
+    invalid: i64, // Total number of unrecognized features (not counted in feats)
+    addresses: i64, // Total number of address points in clusters/orphans
+    intersections: i64, // Total number of address features
+    address_orphans: i64, // Total number of address orphans
+    network_orphans: i64, // Total number of network orphans
+    custom: StatsCustom
+}
+
+impl Stats {
+    fn new() -> Self {
+        Stats {
+            feats: 0,
+            clusters: 0,
+            invalid: 0,
+            addresses: 0,
+            intersections: 0,
+            address_orphans: 0,
+            network_orphans: 0,
+            custom: StatsCustom::new()
+        }
+    }
+}
+
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct StatsCustom {
+    postcodes: i64,
+    accuracy: StatsAccuracy
+}
+
+impl StatsCustom {
+    pub fn new() -> Self {
+        StatsCustom {
+            postcodes: 0,
+            accuracy: StatsAccuracy::new()
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct StatsAccuracy {
+    rooftop: i64,
+    parcel: i64,
+    point: i64
+}
+
+impl StatsAccuracy {
+    pub fn new() -> Self {
+        StatsAccuracy {
+            rooftop: 0,
+            parcel: 0,
+            point: 0
+        }
     }
 }
