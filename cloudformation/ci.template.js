17de2ebb4a4269b03c06a02e3f9cfa0684409e59
Matt Ciferri <matt.ciferri@mapbox.com>
2019-08-13 14:54:59 -0400

    add octokit, cf, and template for ci user

---
+++ cloudformation/ci.template.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const cf = require('@mapbox/cloudfriend');
+
+module.exports = {
+    Parameters: {
+        SecretPrefix: {
+            Type: 'String',
+            Description: 'Prefix of GitHub App'
+        }
+    },
+    Resources: {
+        User: {
+            Type: 'AWS::IAM::User',
+            Properties: {
+                PolicyName: 'github-apps',
+                PolicyDocument: {
+                    Statement: [
+                        {
+                            Effect: 'Allow',
+                            Action: 'secretsmanager:GetSecretValue',
+                            Resource: cf.join(':', ['arn:aws:secretsmanager', cf.region, cf.accountId, 'secret', `${cf.ref('SecretPrefix')}/*`])
+                        }
+                    ]
+                }
+            }
+        },
+        AccessKey: {
+            Type: 'AWS::IAM::AccessKey',
+            Properties: {
+                UserName: cf.ref('User')
+            }
+        }
+    },
+    Outputs: {
+        AccessKeyId: {
+            Value: cf.ref('AccessKey')
+        },
+        SecretAccessKey: {
+            Value: cf.getAtt('AccessKey', 'SecretAccessKey')
+        }
+    }
+};
