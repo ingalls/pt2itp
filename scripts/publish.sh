a72a45c092501ee37ddace28d31305edee07dc1b
Matt Ciferri <matt.ciferri@mapbox.com>
2019-08-14 15:12:53 -0400

    add scripts for binary upload

---
+++ scripts/publish.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -eo pipefail
+
+if [[ -z ${CIRCLE_TAG} ]]; then
+  echo "ok - No tag, skipping release.";
+  exit
+fi
+
+echo "ok - Publishing release.";
+
+export NODE_PRE_GYP_GITHUB_TOKEN=$(node ./scripts/token.js ${GITHUB_APP_PREFIX})
+if [[ -z ${NODE_PRE_GYP_GITHUB_TOKEN} ]]; then
+  echo "not ok - Unable to retrieve GitHub token.";
+  exit 1
+fi
+
+yarn package
