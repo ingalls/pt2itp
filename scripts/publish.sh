fdbbab1ba910ae07ad8a16fd47e6025b0dcc1789
Matt Ciferri <matt.ciferri@mapbox.com>
2019-08-14 15:49:55 -0400

    add logging for circle_tag

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,16 +2,17 @@
 
 set -eo pipefail
 
+echo $CIRCLE_TAG
 if [[ -z ${CIRCLE_TAG} ]]; then
-  echo "ok - No tag, skipping release.";
+  echo "ok - No tag, skipping release."
   exit
 fi
 
-echo "ok - Publishing release.";
+echo "ok - Publishing release."
 
 export NODE_PRE_GYP_GITHUB_TOKEN=$(node ./scripts/token.js ${GITHUB_APP_PREFIX})
 if [[ -z ${NODE_PRE_GYP_GITHUB_TOKEN} ]]; then
-  echo "not ok - Unable to retrieve GitHub token.";
+  echo "not ok - Unable to retrieve GitHub token."
   exit 1
 fi
 
