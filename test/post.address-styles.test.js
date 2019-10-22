1b73f50fc259d94b6a2de0f21b9645be197488b5
Matt Ciferri <matt.ciferri@mapbox.com>
2019-10-22 16:13:01 -0400

    handle nullable address styles as standard

--- test/post.address-styles.test.js
+++ test/post.address-styles.test.js
@@ -10,42 +10,84 @@ test('Post: Style', (t) => {
             'carmen:address_style': 'queens',
             'carmen:addressprops': {
                 'carmen:address_style': {
-                    1: 'standard'
+                    3: null,
+                    4: null
                 }
             }
         },
         geometry: {}
     }).properties['carmen:address_styles'].sort(), ['queens', 'standard']
-    , 'returns all address styles under the address_style properties');
+    , 'returns correct styles for a majority of queens styles with some standard');
 
     t.deepEquals(post({
         type: 'Feature',
         properties: {
-            'carmen:address_style': 'standard',
             'carmen:addressprops': {
                 'carmen:address_style': {
-                    1: 'queens',
-                    2: 'queens',
-                    5: 'kings',
-                    6: 'jacks'
+                    0: 'queens',
+                    1: 'queens'
                 }
             }
         },
         geometry: {}
-    }).properties['carmen:address_styles'].sort(), ['jacks', 'kings', 'queens', 'standard']
-    , 'returns all unique address styles if there are several listed in addressprops');
-
-    t.deepEquals(post(), undefined, 'returns undefined if no feature was passed in');
+    }).properties['carmen:address_styles'].sort(), ['queens', 'standard']
+    , 'returns correct styles for a majority of standard styles with some queens');
 
     t.deepEquals(post({
         type: 'Feature',
         properties: {
-            'carmen:address_style': 'standard',
-            'carmen:addressprops': {}
+            'carmen:address_style': 'queens'
         },
         geometry: {}
-    }).properties['carmen:address_styles'], ['standard']
-    , 'returns only the style for address_style if none are under addressprops');
+    }).properties['carmen:address_styles'].sort(), ['queens']
+    , 'returns correct styles for all queens');
+
+    t.deepEquals(post({
+        type: 'Feature',
+        properties: {
+            'carmen:addressprops': {
+                'carmen:address_style': {
+                    3: 'queens',
+                    4: 'kings'
+                }
+            }
+        },
+        geometry: {}
+    }).properties['carmen:address_styles'].sort(), ['kings', 'queens', 'standard']
+    , 'returns correct styles for a majority of standard styles and multiple other custom styles');
+
+    t.deepEquals(post({
+        type: 'Feature',
+        properties: {
+            'carmen:address_style': 'queens',
+            'carmen:addressprops': {
+                'carmen:address_style': {
+                    2: null,
+                    4: 'kings'
+                }
+            }
+        },
+        geometry: {}
+    }).properties['carmen:address_styles'].sort(), ['kings', 'queens', 'standard']
+    , 'returns correct styles for a majority of queens and a mix of standard and custom styles');
+
+    t.deepEquals(post({
+        type: 'Feature',
+        properties: {
+            'carmen:address_style': 'jacks',
+            'carmen:addressprops': {
+                'carmen:address_style': {
+                    1: 'queens',
+                    2: 'kings',
+                    3: 'kings'
+                }
+            }
+        },
+        geometry: {}
+    }).properties['carmen:address_styles'].sort(), ['jacks', 'kings', 'queens']
+    , 'returns correct styles for a mix of custom styles with no standards');
+
+    t.deepEquals(post(), undefined, 'returns undefined if no feature was passed in');
 
     t.deepEquals(post({
         type: 'Feature',
