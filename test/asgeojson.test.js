a42e7b9eb5cd7903c7262ba55e36139b78c5dcdf
Jeff Miccolis <jeff@miccolis.net>
2020-01-02 13:51:41 -0500

    linting

--- test/asgeojson.test.js
+++ test/asgeojson.test.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const test = require('tape');
 const asGeoJSON = require('../lib/map/asgeojson');
 const turf = require('@turf/turf');
@@ -5,47 +7,47 @@ const turf = require('@turf/turf');
 test('mergeFeatures - composition', (t) => {
 
     const itp = turf.multiLineString([[
-        [ -71.31046691448468, 42.701550719883855 ],
-        [ -71.3101749, 42.7012937 ]
+        [-71.31046691448468, 42.701550719883855],
+        [-71.3101749, 42.7012937]
     ]]);
 
     const pts = turf.multiPoint([
-        [ -71.31014227867126, 42.70150325754059 ]
+        [-71.31014227867126, 42.70150325754059]
     ], {
-        'carmen:addressnumber': [ 5 ]
+        'carmen:addressnumber': [5]
     });
 
     const intersections = turf.multiPoint([
-        [ -71.3101749, 42.7012937 ]
+        [-71.3101749, 42.7012937]
     ], {
-        'carmen:intersections': [ 'Cross Street' ]
+        'carmen:intersections': ['Cross Street']
     });
 
     let feature = asGeoJSON.mergeFeatures(itp);
 
     t.equal(feature.geometry.type, 'GeometryCollection');
-    t.equal(feature.geometry.geometries.length, 1)
+    t.equal(feature.geometry.geometries.length, 1);
     t.equal(feature.properties['carmen:addressnumber'], undefined);
     t.equal(feature.properties['carmen:intersections'], undefined);
 
     feature = asGeoJSON.mergeFeatures(itp, pts);
 
     t.equal(feature.geometry.type, 'GeometryCollection');
-    t.equal(feature.geometry.geometries.length, 2)
+    t.equal(feature.geometry.geometries.length, 2);
     t.deepEqual(feature.properties['carmen:addressnumber'], [null, [5]]);
     t.equal(feature.properties['carmen:intersections'], undefined);
 
     feature = asGeoJSON.mergeFeatures(itp, undefined, intersections);
 
     t.equal(feature.geometry.type, 'GeometryCollection');
-    t.equal(feature.geometry.geometries.length, 2)
+    t.equal(feature.geometry.geometries.length, 2);
     t.equal(feature.properties['carmen:addressnumber'], undefined);
     t.deepEqual(feature.properties['carmen:intersections'], [null, ['Cross Street']]);
 
     feature = asGeoJSON.mergeFeatures(itp, pts, intersections);
 
     t.equal(feature.geometry.type, 'GeometryCollection');
-    t.equal(feature.geometry.geometries.length, 3)
+    t.equal(feature.geometry.geometries.length, 3);
     t.deepEqual(feature.properties['carmen:addressnumber'], [null, [5], null]);
     t.deepEqual(feature.properties['carmen:intersections'], [null, null, ['Cross Street']]);
 
@@ -56,7 +58,7 @@ test('mergeFeatures - composition', (t) => {
 test('addressPointsFeature - empty', (t) => {
     const feature = asGeoJSON.addressPointsFeature([]);
     t.equal(feature.geometry.type, 'MultiPoint');
-    t.equal(feature.geometry.coordinates.length, 0)
+    t.equal(feature.geometry.coordinates.length, 0);
     t.end();
 });
 
@@ -70,9 +72,9 @@ test('addressPointsFeature - output: false', (t) => {
         }
     }];
 
-    let feature = asGeoJSON.addressPointsFeature(points);
+    const feature = asGeoJSON.addressPointsFeature(points);
     t.equal(feature.geometry.type, 'MultiPoint');
-    t.equal(feature.geometry.coordinates.length, 0)
+    t.equal(feature.geometry.coordinates.length, 0);
     t.equal(feature.properties['carmen:addressnumber'].length, 0);
 
     t.end();
@@ -91,12 +93,12 @@ test('addressPointsFeature - output: true', (t) => {
         }
     }];
 
-    let feature = asGeoJSON.addressPointsFeature(points);
+    const feature = asGeoJSON.addressPointsFeature(points);
     t.equal(feature.geometry.type, 'MultiPoint');
-    t.equal(feature.geometry.coordinates.length, 1)
+    t.equal(feature.geometry.coordinates.length, 1);
     t.equal(feature.properties['carmen:addressnumber'].length, 1);
     t.equal(feature.properties['carmen:addressnumber'][0], 1);
-    t.deepEqual(feature.properties.address_props, [{number: '2', other: true}]);
+    t.deepEqual(feature.properties.address_props, [{ number: '2', other: true }]);
 
     t.end();
 });
@@ -104,7 +106,7 @@ test('addressPointsFeature - output: true', (t) => {
 test('intersectionFeature - empty', (t) => {
     const feature = asGeoJSON.intersectionFeature([], 0);
     t.equal(feature.geometry.type, 'MultiPoint');
-    t.equal(feature.geometry.coordinates.length, 0)
+    t.equal(feature.geometry.coordinates.length, 0);
     t.end();
 });
 
@@ -131,23 +133,23 @@ test('intersectionFeature - ok', (t) => {
                 priority: 0,
                 display: 'Riverton Road South',
                 tokenized: [{ token: 'riverton', token_type: null }, { token: 'rd', token_type: 'Way' }, { token: 's', token_type: 'Cardinal' }]
-            }],
+            }]
         }
     }];
 
     let feature = asGeoJSON.intersectionFeature(points, 0);
     t.equal(feature.geometry.type, 'MultiPoint');
-    t.equal(feature.geometry.coordinates.length, 0)
+    t.equal(feature.geometry.coordinates.length, 0);
     t.equal(feature.properties['carmen:intersections'].length, 0);
 
     feature = asGeoJSON.intersectionFeature(points, 1);
     t.equal(feature.geometry.type, 'MultiPoint',);
     t.equal(feature.properties['carmen:intersections'].join(), 'WV Route 33,Mountaineer Drive');
-    t.equal(feature.geometry.coordinates.length, 2)
+    t.equal(feature.geometry.coordinates.length, 2);
 
     feature = asGeoJSON.intersectionFeature(points, 3);
     t.equal(feature.geometry.type, 'MultiPoint');
     t.equal(feature.properties['carmen:intersections'].join(), 'Riverton Road South');
-    t.equal(feature.geometry.coordinates.length, 1)
+    t.equal(feature.geometry.coordinates.length, 1);
     t.end();
 });
