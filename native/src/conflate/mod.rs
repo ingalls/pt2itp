19a8284367622a6bfbf0812ac831f1b06d19bfd5
mapbox-danny <daniel.kleinman@mapbox.com>
2020-09-08 15:36:51 -0400

    Add 'interpolate' column for conflation

--- native/src/conflate/mod.rs
+++ native/src/conflate/mod.rs
@@ -93,6 +93,7 @@ pub fn conflate(mut cx: FunctionContext) -> JsResult<JsBoolean> {
             number TEXT,
             source TEXT,
             output BOOLEAN,
+            interpolate BOOLEAN,
             props JSONB,
             geom GEOMETRY(POINT, 4326)
         );
